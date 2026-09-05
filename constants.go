849bed9484a2d8aa925d0f1277e7894c49ec1f04
Honore Jabiro <honorejabiro@gmail.com>
2026-09-05 14:58:13 -0500

    defined constants

---
+++ constants.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const a string = "constant"
+
+func main() {
+	fmt.Println(a)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+}
