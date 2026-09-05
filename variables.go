a48a588f603cb898ec8fb16df33ed99116231b48
Honore Jabiro <honorejabiro@gmail.com>
2026-09-05 14:18:02 -0500

    defining variables

---
+++ variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "initial"
+	fmt.Println(a)
+
+	var b, c int = 6, 10
+	fmt.Println(b * c)
+
+	d := true
+	e := false
+
+	fmt.Println(d && e)
+
+	f := "apple"
+	fmt.Println(f)
+}
